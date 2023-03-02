dd121794810c1199e28b249cccd3dd773d72d621
davidlopezsotelo <davidaquarium@yahoo.es>
2023-03-02 14:32:19 +0100

    App Aquarium 1.1.0 mejora de vistas

--- app/src/main/java/com/example/acuariumapp/Menu.kt
+++ app/src/main/java/com/example/acuariumapp/Menu.kt
@@ -1,8 +1,10 @@
 package com.example.acuariumapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -52,7 +54,20 @@ class Menu : AppCompatActivity() {
     }
     fun salir(vista:View?){
 
+
+        val myToast=Toast.makeText(this,"Hasta la vista, baby....",Toast.LENGTH_LONG)
+        myToast.setGravity(Gravity.CENTER,0,0)
+        myToast.show()
+
+
       finish()
+
+       /* fun myToast (contexto: Context,mensaje:String){
+
+            val myToast=Toast.makeText(this,"Hasta la vista, baby....",Toast.LENGTH_LONG)
+            myToast.setGravity(Gravity.CENTER,0,0)
+            myToast.show()
+        }*/
 
 
 
